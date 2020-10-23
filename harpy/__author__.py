f7e111716028d249278fc2ed0e481c6f3742f5de
Serhat Çelik <71452635+serhatcelik@users.noreply.github.com>
2020-10-23 07:08:54 +0300

    Add files via upload

---
+++ harpy/__author__.py
@@ -0,0 +1,9 @@
+# This file is part of hARPy
+# Released under the MIT license
+# Copyright (c) Serhat Çelik
+
+"""Program author information."""
+
+__author__ = 'Serhat Çelik'
+__email__ = 'prjctsrht@gmail.com'
+__gitlink__ = 'https://github.com/serhatcelik'
