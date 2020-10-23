7156f04e7239f976a90205cf0f977f01adfab59f
Serhat Çelik <71452635+serhatcelik@users.noreply.github.com>
2020-10-23 07:06:17 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,40 +1,45 @@
 # This file is part of hARPy
+# Released under the MIT license
+# Copyright (c) Serhat Çelik
+
+"""Setup script."""
 
 import setuptools
 from harpy.__version__ import __version__
+from harpy.__author__ import __author__, __email__, __gitlink__
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="harpy-prjct",
+    name='harpy-prjct',
     version=__version__,
-    author="Serhat Celik",
-    author_email="prjctsrht@gmail.com",
-    url="https://github.com/serhatcelik",
-    description="Harpy who is using ARP",
+    author=__author__,
+    author_email=__email__,
+    url=__gitlink__,
+    description='Harpy who is using ARP',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     packages=setuptools.find_packages(),
     classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 3"
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3 :: Only'
     ],
-    python_requires=">=3.6",
+    python_requires='>=3.6',
     package_data={
-        "": [
-            "*.json"
+        '': [
+            '*.json'
         ]
     },
     entry_points={
-        "console_scripts": [
-            "harpy = harpy.__main__:main"
+        'console_scripts': [
+            'harpy = harpy.__main__:main'
         ]
     },
     options={
-        "build_scripts": {
-            "executable": "/bin/custom_python"
+        'build_scripts': {
+            'executable': '/bin/custom_python'
         }
     }
 )
