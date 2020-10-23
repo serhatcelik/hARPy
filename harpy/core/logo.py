b79393a00461ac4d554150038b588000701dfd25
Serhat Çelik <71452635+serhatcelik@users.noreply.github.com>
2020-10-23 07:11:58 +0300

    Add files via upload

---
+++ harpy/core/logo.py
@@ -0,0 +1,25 @@
+# This file is part of hARPy
+# Released under the MIT license
+# Copyright (c) Serhat Çelik
+
+"""Module for handling the program logo."""
+
+
+def create_logo():
+    """Create the program logo."""
+
+    return [
+        r'|_  _  _ _   ',
+        r'| |(_|| |_)\/',
+        r'        |  / '
+    ]
+
+
+def create_banner():
+    """Create a banner for the program logo."""
+
+    return [
+        'TOTAL HOST: ',
+        'TOTAL REQ.: ',
+        'TOTAL REP.: '
+    ]
