69a2d02ee8a6d7586875edee9f509f1f24f6d487
Serhat Çelik <71452635+serhatcelik@users.noreply.github.com>
2020-11-01 06:54:04 +0300

    Add files via upload

--- harpy/__main__.py
+++ harpy/__main__.py
@@ -10,7 +10,8 @@ import time
 import atexit
 import traceback
 import logging.handlers
-import harpy.core.data as data
+import harpy.data.core as core
+import harpy.data.functions as func
 from harpy.threads.send_thread import SendThread
 from harpy.threads.sniff_thread import SniffThread
 from harpy.handlers.echo_handler import EchoHandler
@@ -26,36 +27,29 @@ def terminator():
     """Terminate all threads."""
 
     # Disable the handler to prevent activating it again
-    SignalHandler(*data.SIGNALS).disable()
+    SignalHandler(*core.SIGNALS).disable()
 
     # Not hanged up?
     while sys.stdout.isatty():
         print('\n')
-        if data.ERRORS:
-            for _ in data.ERRORS:
-                print(_)
-            print()
-
-        if data.TIMED_OUT:
-            print('timed out,', end=' ')
-        elif data.SIGNAL_NUMBER is not None:
-            print('received signal {},'.format(data.SIGNAL_NUMBER), end=' ')
+        for _ in core.EXIT_MESSAGES:
+            print(_, end='')
         print('cleaning...\n', flush=True)
         break
 
-    for i, _ in enumerate(data.THREADS):
+    for i, _ in enumerate(core.THREADS):
         vars(main)[_].flag.set()
         time_terminator = time.time()  # Restore for every thread
         while vars(main)[_].is_alive():
             # Still alive?
-            if time.time() - time_terminator >= data.SLEEP_TERMINATOR:
+            if time.time() - time_terminator >= core.SLEEP_TERMINATOR:
                 # Last thread?
-                if i == len(data.THREADS) - 1:
+                if i == len(core.THREADS) - 1:
                     hard_terminator()  # Force to exit without logging
                 else:
                     break
 
-    vars(main)[data.SOCKET].close()  # Close the socket
+    vars(main)[core.SOCKET].close()  # Close the socket
 
 
 def hard_terminator(*args):
@@ -68,7 +62,7 @@ def hard_terminator(*args):
     # Container full?
     if args:
         logger = logging.getLogger()
-        logger.addHandler(logging.handlers.SysLogHandler(address='/dev/log'))
+        logger.addHandler(logging.handlers.SysLogHandler(address=core.DEV_LOG))
         logger.critical(traceback.format_exception(args[0], args[1], args[-1]))
 
     # atexit.register will not work when os._exit() is called, so...
@@ -88,57 +82,58 @@ def main():
     elif os.getpgrp() != os.tcgetpgrp(sys.stdout.fileno()):
         sys.exit(1)
 
-    SignalHandler(data.SIGHUP).enable(data.signal_handler)
-    SignalHandler(data.SIGCHLD).disable()
-    SignalHandler(data.SIGWINCH).disable()
-    SignalHandler(*data.SIGNALS).disable()
+    SignalHandler(core.SIGHUP).enable(func.signal_handler)
+    SignalHandler(core.SIGCHLD).disable()
+    SignalHandler(core.SIGWINCH).disable()
+    SignalHandler(*core.SIGNALS).disable()
 
     # Register to enable the terminal echo at normal program termination
     atexit.register(EchoHandler().enable)
     EchoHandler().disable()
 
     parser = ParserHandler.add_arguments()
-    # No arguments?
-    if len(sys.argv) == 1:
+    core.COMMANDS = parser.parse_args()
+    if True in [
+            not core.COMMANDS.p and not core.COMMANDS.r,
+            core.COMMANDS.f and not core.COMMANDS.r
+    ]:
         sys.exit(parser.print_help())
-
-    data.COMMANDS = parser.parse_args()
-    if False in ParserHandler.check_arguments(data.COMMANDS):
+    elif False in ParserHandler.check_arguments(core.COMMANDS):
         sys.exit(1)
 
-    setattr(main, data.SOCKET, SocketHandler(data.SOC_PRO))  # Open a socket
-    vars(main)[data.SOCKET].set_options()
-    vars(main)[data.SOCKET].bind(interface=data.COMMANDS.i, port=data.SOC_POR)
+    setattr(main, core.SOCKET, SocketHandler(core.SOC_PRO))  # Open a socket
+    vars(main)[core.SOCKET].set_options()
+    vars(main)[core.SOCKET].bind(interface=core.COMMANDS.i, port=core.SOC_POR)
 
-    data.ETH_SRC = InterfaceHandler.get_mac(vars(main)[data.SOCKET].raw_soc)
-    data.ARP_SND = data.ETH_SRC
-    data.COMMANDS.r = data.new_range()
+    core.ETH_SRC = InterfaceHandler.get_mac(vars(main)[core.SOCKET].l2soc)
+    core.ARP_SND = core.ETH_SRC
+    core.COMMANDS.r = func.new_range()
 
-    setattr(main, data.SNIFF, SniffThread(vars(main)[data.SOCKET].raw_soc))
-    data.THREADS.append(vars(main)[data.SNIFF].name)
-    vars(main)[data.SNIFF].start()  # Start sniffing packets
+    setattr(main, core.SNIFF, SniffThread(vars(main)[core.SOCKET].l2soc))
+    core.THREADS.append(vars(main)[core.SNIFF].name)
+    vars(main)[core.SNIFF].start()  # Start sniffing packets
 
     # Active mode?
-    if not data.COMMANDS.p:
-        setattr(main, data.SEND, SendThread(vars(main)[data.SOCKET].raw_soc))
-        data.THREADS.append(vars(main)[data.SEND].name)
-        vars(main)[data.SEND].start()  # Start sending packets
+    if not core.COMMANDS.p:
+        setattr(main, core.SEND, SendThread(vars(main)[core.SOCKET].l2soc))
+        core.THREADS.append(vars(main)[core.SEND].name)
+        vars(main)[core.SEND].start()  # Start sending packets
 
-    while data.RUN_MAIN:
-        SignalHandler(*data.SIGNALS).disable()
-        window = WindowHandler(data.SNIFF_RESULTS)
+    while core.RUN_MAIN:
+        SignalHandler(*core.SIGNALS).disable()
+        window = WindowHandler(core.SNIFF_RESULTS)
         os.system('clear')
         window.draw_skeleton()
         window()
-        SignalHandler(*data.SIGNALS).enable(data.signal_handler)
+        SignalHandler(*core.SIGNALS).enable(func.signal_handler)
 
         time_main = time.time()  # Restore after a certain period of time
-        while data.RUN_MAIN and time.time() - time_main < data.SLEEP_MAIN:
-            data.run_main()
-            if data.SNIFF_RESULT:
-                data.SNIFF_RESULTS = ResultHandler(
-                    result=data.SNIFF_RESULT.pop(0),
-                    results=data.SNIFF_RESULTS
+        while core.RUN_MAIN and time.time() - time_main < core.SLEEP_MAIN:
+            func.run_main()
+            if core.SNIFF_RESULT:
+                core.SNIFF_RESULTS = ResultHandler(
+                    result=core.SNIFF_RESULT.pop(0),
+                    results=core.SNIFF_RESULTS
                 )()
 
 
