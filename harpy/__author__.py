a453081e803322c0df0e6014e2009ad354cefeab
Serhat Çelik <71452635+serhatcelik@users.noreply.github.com>
2020-11-21 02:57:30 +0300

    Delete __author__.py

--- harpy/__author__.py
+++
@@ -1,9 +0,0 @@
-# This file is part of hARPy
-# Released under the MIT license
-# Copyright (c) Serhat Çelik
-
-"""Program author information."""
-
-__author__ = 'Serhat Çelik'
-__email__ = 'prjctsrht@gmail.com'
-__gitlink__ = 'https://github.com/serhatcelik'
